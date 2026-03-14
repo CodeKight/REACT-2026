a280603c99fba3499a22b2077f96d5c29e8f4151
CodeKnight <dearjinni44@gmail.com>
2026-03-14 21:09:46 +0545

    react basic (import and export)

---
+++ app.js
@@ -0,0 +1,7 @@
+
+//import and export
+const name = "Gems"
+export const age = "23"
+export const skills = ["frontend", "backend", "ui/ux"]
+
+export default name
---
+++ script.js
@@ -0,0 +1,7 @@
+
+//import and export
+import name from './app.js'
+import {age} from './app.js'
+import {skills} from './app.js'
+
+console.log(name, age , skills)
