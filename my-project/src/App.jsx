22c8fe4b4b7da3095820ffa8374ac2a04c3e91cb
CodeKnight <dearjinni44@gmail.com>
2026-03-16 10:50:23 +0545

    created react folder with vite (passed html tag to return statement, passed multiple tag using empty element, used concept of fragments)

---
+++ my-project/src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+  // return 'hello, what are you doing?'
+
+  // - you can return html element also
+
+  return (
+    <>
+      <div>
+        <h1> Hello h1</h1>
+        <h2> Hello h2</h2>
+      </div>
+
+      <div>                 
+        <h3> Hello h3</h3>
+        <h4> Hello h4</h4>
+      </div>
+    </>
+  );  // - these was the use of fragments.
+}  
+
+export default App;
