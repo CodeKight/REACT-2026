9ca5d19993f34e14c063c6c063eb4690c5a86d10
CodeKnight <dearjinni44@gmail.com>
2026-03-19 17:44:31 +0545

    integrating tailwind css with react/vite

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // <div className='bg-red-600'>App</div>
+    <div className='h-screen w-full bg-gray-900'>App</div>
+  )
+}
+
+export default App
